3218eb3ed2390751066d97411502e654a7245098
Gabriel Renato <gabrielrenatovga@yahoo.com>
2022-02-11 11:08:04 -0300

    Nova Atualização Vercel Frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://projetowebcefetmg.herokuapp.com/,
+  baseURL: "https://projetowebcefetmg.herokuapp.com/",
   headers: {
     'Content-Type': 'application/json'
   }
